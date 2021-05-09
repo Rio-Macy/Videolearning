aad6da2b83c4e4bd3e4735e0cdaa023c54e89075
sujuntao <1423957197@qq.com>
2021-05-09 10:34:09 +0800

    面试题

---
+++ src/test/java/com/duyi/test/TestNiuCoder.java
@@ -0,0 +1,20 @@
+package com.duyi.test;
+
+public class TestNiuCoder {
+
+    private static int x = 10;
+    public static void main(String[] args) {
+
+//        TestNiuCoder niuCoder = new TestNiuCoder();
+//        niuCoder.x++;
+//        TestNiuCoder niuCoder1 = new TestNiuCoder();
+//        niuCoder1.x++;
+//
+//        niuCoder = new TestNiuCoder();
+//        niuCoder.x++;
+//        System.out.println(x);
+
+        StaticNumber staticNumber = new StaticNumber();
+
+    }
+}
