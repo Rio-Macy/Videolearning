77ed65bd9b30f749efe65e09eaf1c12e40c0ec04
sujuntao <1423957197@qq.com>
2021-05-09 17:19:14 +0800

    第一次上传

--- src/test/java/com/duyi/test/RunMan.java
+++ src/test/java/com/duyi/test/RunMan.java
@@ -18,7 +18,13 @@ public class RunMan {
         return name;
     }
 
+    public void sun() {
 
+        for (int i = 0; i < 100; i++) {
+
+            System.out.println(i);
+        }
+    }
     public void setName(String name) {
         this.name = name;
     }
