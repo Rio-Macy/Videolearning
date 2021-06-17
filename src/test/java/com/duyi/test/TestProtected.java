7d4516cf4690015e13a505bb7803e6af6c7866b2
sujuntao <1423957197@qq.com>
2021-06-17 20:34:25 +0800

    第一次上传

---
+++ src/test/java/com/duyi/test/TestProtected.java
@@ -0,0 +1,11 @@
+package com.duyi.test;
+
+import com.duyi.test.ptest.Son;
+
+public class TestProtected {
+
+    public static void main(String[] args) {
+        Son son = new Son();
+//        son.fatherProtectedFun();
+    }
+}
